7140956c1f2cf216bbbdc2218cfe709296e2262d
nmcke1 <nmckenna123@gmail.com>
2025-07-31 16:33:24 +0100

    Adding basic implementation for test server

---
+++ LoadBalancer/LoadBalancer/Services/TestServer.cs
@@ -0,0 +1,22 @@
+﻿using LoadBalancer.Interfaces;
+
+namespace LoadBalancer.Services
+{
+    internal class TestServer(string address, int port) : IServer
+    {
+        // Properties
+        public string Address { get; } = address;
+        public int Port { get; } = port;
+
+        // IServer Implementations
+        public async Task StartUp()
+        {
+            Console.WriteLine($"Starting test server at {Address}:{Port}");
+            await Task.Delay(1000); // Simulate async operation
+        }
+
+        public string toString() {
+            return $"\nAddress: {Address} \t Port: {Port}";
+        }
+    }
+}
