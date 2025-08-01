e819ec74228e302ebb1328082efd76094862cc50
nmcke1 <nmckenna123@gmail.com>
2025-08-01 22:42:58 +0100

    Adding a dummy client to make requests to the load balancer

---
+++ LoadBalancer/LoadBalancer/Interfaces/IClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadBalancer.Interfaces
+{
+    internal interface IClient
+    {
+        /// <summary>
+        /// Connects to a specified IP and reads the response.
+        /// </summary>
+        /// <returns>Returns the response from the TCP connection.</returns>
+        public string ConnectAndReadResponse();
+    }
+}
