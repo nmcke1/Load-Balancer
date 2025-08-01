a5ef144d6686f75c469cd3334fa1ebb0f102d0c5
nmcke1 <nmckenna123@gmail.com>
2025-08-01 17:27:10 +0100

    Basic implementations for the simple methods of the ILoadBalancer interface

---
+++ LoadBalancer/LoadBalancer/Services/LoadBalancerService.cs
@@ -0,0 +1,53 @@
+﻿using LoadBalancer.Interfaces;
+using LoadBalancer.RoundRobin;
+
+namespace LoadBalancer.Services
+{
+    internal class LoadBalancerService : ILoadBalancer
+    {
+        private readonly RoundRobinList serverList = new();
+
+        public LoadBalancerService(IServer[] servers)
+        {
+            foreach(var server in servers)
+            {
+                AddServer(server);
+            }
+        }
+
+        public void AddServer(IServer server)
+        {
+            serverList.AppendList(server);
+        }
+
+        public IServer NextServer()
+        {
+            return serverList.NextNode();
+        }
+
+        public bool RemoveServer(IServer server)
+        {
+            return serverList.RemoveNode(server);
+        }
+
+        public string PrintServers()
+        {
+            return serverList.ToString();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HealthCheck()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
