dc3e36a92005d37a787a18e8dd04d4e2f11e8527
nmcke1 <nmckenna123@gmail.com>
2025-08-01 18:54:02 +0100

    Adding new interface for client handlers

---
+++ LoadBalancer/LoadBalancer/Interfaces/IClientHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadBalancer.Interfaces
+{
+    internal interface IClientHandler
+    {
+        /// <summary>
+        /// Handles communication with a connected TCP client.
+        /// </summary>
+        /// <param name="client"> The connected <see cref="TcpClient"/> to handle. </param>
+        /// <returns> A task representing the asynchronous operation. </returns>
+        Task HandleAsync(TcpClient client);
+    }
+}
